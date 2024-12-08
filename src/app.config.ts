85d28d6dbbad0cad297867408bf6c3833f9ef0c8
Luca Peruzzo <peruzzoluca98@gmail.com>
2024-12-08 16:26:06 +0100

    account api poc

--- src/app.config.ts
+++ src/app.config.ts
@@ -1,6 +1,11 @@
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import type { ApplicationConfig } from '@angular/core';
 import { provideExperimentalZonelessChangeDetection } from '@angular/core';
+import { authenticationInterceptor } from './account/api/client.interceptor';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideExperimentalZonelessChangeDetection()],
+  providers: [
+    provideExperimentalZonelessChangeDetection(),
+    provideHttpClient(withInterceptors([authenticationInterceptor])),
+  ],
 };
