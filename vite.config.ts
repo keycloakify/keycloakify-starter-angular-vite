cf77a77429e6b795d2e7d0fd8ae302b2993f4f66
Luca Peruzzo <peruzzoluca98@gmail.com>
2025-07-29 22:14:26 +0200

    chore: update dependencies and clean test files

--- vite.config.ts
+++ vite.config.ts
@@ -18,14 +18,4 @@ export default defineConfig(({ mode }) => ({
       accountThemeImplementation: 'none',
     }),
   ],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: ['src/test-setup.ts'],
-    include: ['**/*.spec.ts'],
-    reporters: ['default'],
-  },
-  define: {
-    'import.meta.vitest': mode !== 'production',
-  },
 }));
