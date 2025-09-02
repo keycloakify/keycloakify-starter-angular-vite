25eb3b59bdbdad6c9883254b5c4a5033ec592b6d
Luca Peruzzo <peruzzoluca98@gmail.com>
2025-09-02 08:54:51 +0200

    feat: add style placeholder comment

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,6 +1,12 @@
 import 'zone.js';
 import { type Preview } from '@analogjs/storybook-angular';
 
+/** import here your global style if needed
+ *
+ * import '../src/styles.css';
+ *
+ */
+
 const preview: Preview = {
   parameters: {
     controls: {
