12d3c2c9ea169c8e003d5f42addd0d7e7cc8d2b2
Luca Peruzzo <peruzzoluca98@gmail.com>
2026-06-06 11:51:48 +0200

    feat: update to angular 22

--- src/no-context.component.ts
+++ src/no-context.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'kc-root',
   standalone: true,
+  changeDetection: ChangeDetectionStrategy.OnPush,
   template: `<h1>No Keycloak Context</h1>`,
 })
 export class NoContextComponent {}
